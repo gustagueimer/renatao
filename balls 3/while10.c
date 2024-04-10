97699419f1a3ca3ed29da084c4d3cce20abfcc96
gustagueimer <gustavomarqueslima@outlook.com>
2024-04-10 18:59:03 -0300

    faz o L

--- balls 3/while10.c
+++ balls 3/while10.c
@@ -1,15 +1,16 @@
 #include <stdio.h>
 #include <windows.h>
 int main () {
-    int c[2];
+    float c[2];
     c[1] = 200;
+    c[2] = 0;
     printf("impressor de uma media de numeros entre 200 a 350 deluxe\n");
-    while(c[1] < 351) {
+    while(c[1] <=350) {
         c[2] = c[2] + c[1];
         c[1]++;
     }
-    c[1] = c[2] / 150;
-    printf("resultado final: %i\n", c[1]);
+    c[1] = c[2] / 151;
+    printf("resultado final: %.2f\n", c[1]);
     system("pause");
     return 0;
 }
---
+++ balls 3/while13.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <windows.h>
+int main () {
+    float c[4];
+    printf("impressor de uma media de numeros entre uma faixa definida pelo usuario deluxe\n");
+    printf("insira o numero de inicio\n");
+    scanf("%f", &c[1]);
+    c[2] = 0;
+    c[4] = c[1];
+    printf("insira o numero de final\n");
+    scanf("%f", &c[3]);
+    while(c[1] <= c[3]) {
+        c[2] = c[2] + c[1];
+        printf("c1: %.2f\n", c[1]);
+        printf("c2: %.2f\n", c[2]);
+        c[1]++;
+    }
+    printf("c1: %.2f\n", c[1]);
+    printf("c2: %.2f\n", c[2]);
+    printf("c3: %.2f\n", c[3]);
+    printf("c4: %.2f\n", c[4]);
+    c[1] = c[2] / ((c[3] - c[4]) + 1);
+    printf("resultado final: %.2f\n", c[1]);
+    system("pause");
+    return 0;
+}
